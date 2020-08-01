934200e39d4432e5446a070c77fc0c4ffda11163
Olivier Pelletier <opetgamers@gmail.com>
2020-08-01 11:18:59 -0400

    Fixing beetle moves.

--- src/engine/rules/moves.rs
+++ src/engine/rules/moves.rs
@@ -22,7 +22,10 @@ pub fn beetle_moves(grid: &Grid, hex: &Hex) -> Vec<Hex> {
   let mut moves: Vec<Hex> = Vec::new();
 
   for neighbor in hex.neighbors() {
-    if one_hive_rule(grid, hex, &neighbor) && freedom_to_move_rule(grid, hex, &neighbor) {
+    if one_hive_rule(grid, hex, &neighbor)
+      && ((!grid.is_hex_occupied(hex) && freedom_to_move_rule(grid, hex, &neighbor))
+        || grid.is_hex_occupied(hex))
+    {
       moves.push(neighbor);
     }
   }
