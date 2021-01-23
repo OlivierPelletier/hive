482b2a0865b00e35b93a32fe8b4f4ff825c6491c
Olivier Pelletier <olivier.pelletier@levio.ca>
2021-01-23 07:51:52 -0500

    Adding some idea to game.rs.

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -1,1 +1,22 @@
-pub struct Game {}
+use crate::engine::grid::coordinate::hex::Hex;
+use crate::engine::grid::piece::{Piece, PieceColor};
+use crate::engine::grid::Grid;
+
+pub struct Game {
+  pub grid: Grid,
+  pub players: Vec<Player>,
+  pub moves: Vec<Move>,
+  pub turn: u64,
+}
+
+pub struct Player {
+  pub color: PieceColor,
+  pub pieces: Vec<Piece>,
+  pub is_queen_played: bool,
+}
+
+pub struct Move {
+  pub piece: Piece,
+  pub from: Hex,
+  pub to: Hex,
+}
